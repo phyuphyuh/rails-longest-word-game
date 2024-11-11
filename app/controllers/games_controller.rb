988151628b294daf61daf011d2427630707e4a81
Phyu Phyu Hlaing <phyuphyu.h27@gmail.com>
2024-11-11 15:51:23 +0900

    done

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,34 @@
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+  end
+
+  def score
+    @result = if included(params[:word], params[:letters]) && valid(params[:word])
+                "Congratulations! #{params[:word]} is a valid English word!"
+              elsif !included(params[:word], params[:letters])
+                "Sorry but #{params[:word]} cannot be built out of #{params[:letters]}"
+              else
+                "Sorry but #{params[:word]} does not seem to be a valid English word..."
+              end
+  end
+
+  def included(word, grid)
+    # params[:word].chars.all? do |letter|
+    #   params[:letters].include?(letter)
+    # end
+    word.chars.all? do |letter|
+      grid.downcase.include?(letter)
+    end
+  end
+
+  def valid(word)
+    # url = "https://dictionary.lewagon.com/#{params[:word]}"
+    url = "https://dictionary.lewagon.com/#{word}"
+    json = URI.open(url).read
+    results = JSON.parse(json)
+    @found = results['found']
+  end
+end
