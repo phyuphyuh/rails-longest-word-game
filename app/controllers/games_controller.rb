26d7a7474f661ce5d9df0b25dd4d9de13543a156
Phyu Phyu Hlaing <phyuphyu.h27@gmail.com>
2024-11-11 16:41:27 +0900

    refactored

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,14 +5,28 @@ class GamesController < ApplicationController
     @letters = Array.new(10) { ('A'..'Z').to_a.sample }
   end
 
+  # def score
+  #   @result = if included(params[:word], params[:letters]) && valid(params[:word])
+  #               "Congratulations! #{params[:word]} is a valid English word!"
+  #             elsif !included(params[:word], params[:letters])
+  #               "Sorry but #{params[:word]} cannot be built out of #{params[:letters]}"
+  #             else
+  #               "Sorry but #{params[:word]} does not seem to be a valid English word..."
+  #             end
+  # end
+
   def score
-    @result = if included(params[:word], params[:letters]) && valid(params[:word])
-                "Congratulations! #{params[:word]} is a valid English word!"
-              elsif !included(params[:word], params[:letters])
-                "Sorry but #{params[:word]} cannot be built out of #{params[:letters]}"
-              else
-                "Sorry but #{params[:word]} does not seem to be a valid English word..."
-              end
+    @result = generate_score(params[:word], params[:letters])
+  end
+
+  def generate_score(word, grid)
+    if included(word, grid) && valid(word)
+      "Congratulations! #{word} is a valid English word!"
+    elsif !included(word, grid)
+      "Sorry but #{word} cannot be built out of #{grid}"
+    else
+      "Sorry but #{word} does not seem to be a valid English word..."
+    end
   end
 
   def included(word, grid)
